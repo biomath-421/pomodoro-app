b431907b2f3e2744d3eb87b1a1026977593a821a
Mathieu Bourlard <mathieubourlard01@gmail.com>
2020-09-16 15:54:56 +0200

    test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ By Mathieu*/
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Index from "./components/root";
+import BrInterval from "./components/BrInterval";
+import sessionLength from "./components/SessionLength";
+import Timer from "./components/Timer";
 
 ReactDOM.render(
     <Index />,
