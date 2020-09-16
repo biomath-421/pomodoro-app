646b9a11b96bce6fc2ab522d531a2c10afbf7ec9
Mathieu Bourlard <mathieubourlard01@gmail.com>
2020-09-16 15:17:48 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom";
 import Index from "./components/root";
 import BrInterval from "./components/BrInterval";
 import sessionLength from "./components/SessionLength";
-import Timer from "./components/Timer";
+import Timer from "../src/components/Timer";
 
 ReactDOM.render(
     <Index />,
